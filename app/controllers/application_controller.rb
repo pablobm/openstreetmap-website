76bf81bb2e2bf38d219c70336334b9fc927329c1
Marwin Hochfelsner <50826859+hlfan@users.noreply.github.com>
2025-11-03 19:39:02 +0100

    Precompute color scheme fallback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -274,12 +274,14 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def preferred_color_scheme(subject)
-    if current_user
-      current_user.preferences.find_by(:k => "#{subject}.color_scheme")&.v || "auto"
-    else
-      "auto"
+  def preferred_color_scheme(*subjects)
+    return nil unless current_user
+
+    subjects.each do |subject|
+      scheme = current_user.preferences.find_by(:k => "#{subject}.color_scheme")&.v
+      return scheme unless scheme.nil? || scheme == "auto"
     end
+    nil
   end
 
   helper_method :preferred_editor, :preferred_color_scheme
