45cad582cab82db210a093aac7e635cc28710c5a
Pablo Brasero <pablo@pablobm.com>
2026-07-14 18:23:00 +0100

    Avoid showing very long pieces of text in notifications
    I am unsure about this one. I am relying on RichText#truncate_html,
    to avoid having to put together my own truncation facility. Note that it
    wouldn't be as simple as using Rails's own String#truncate as this is a
    RichText, not a String, and then I'd have to worry about truncating the
    HTML.
    
    I find RichText#truncate_html to be too coarse-grained. If I understand
    it right, it removes entire HTML elements. If the text starts with a
    small paragraph, then a long one, it will only show the first. If it
    starts with a long paragraph, it may not show anything, or it will show
    the whole paragraph. I think this will be confusing to readers.

--- test/controllers/notifications/note_comment_view_test.rb
+++ test/controllers/notifications/note_comment_view_test.rb
@@ -29,5 +29,27 @@ module Notifications
         assert_dom "blockquote", note_comment.body
       end
     end
+
+    def test_render_with_long_text
+      comment_author = build_stubbed(
+        :user,
+        :display_name => "Helpful Commenter"
+      )
+      note = build_stubbed(:note)
+      note_comment = build_stubbed(
+        :note_comment,
+        :author => comment_author,
+        :note => note,
+        :body => read_fixture_file("lorem_ipsum.txt")
+      )
+      notification = build_stubbed(:notification, :record => note_comment)
+
+      render(
+        "notifications/notification",
+        :notification => notification
+      )
+
+      assert_dom ".web-notification blockquote p", :count => 3
+    end
   end
 end
